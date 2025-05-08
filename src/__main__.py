b46aa84ddb2d5e949d07b0b6268620415b86ad17
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-07 22:27:23 -0300

    rename main to __main__

--- src/__main__.py
+++ src/__main__.py
@@ -3,6 +3,7 @@ from src.database.database_creation import DatabaseCreation
 from src.database.database_config import database_config
 
 if __name__ == "__main__":
+    print("hello")
     database_creation = DatabaseCreation()
     database_config.run_migrations()
     directory_scanning.scanning()
