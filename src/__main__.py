00db5fdc4af601f786b24e6c58b7917e7b3527df
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-10 18:34:42 -0300

    remove dumb log

--- src/__main__.py
+++ src/__main__.py
@@ -3,7 +3,6 @@ from src.database.database_creation import new_database
 from src.pipeline_etl.extract.dir_scanning import directory_scanning
 
 if __name__ == "__main__":
-    print("hello")
     new_database()
     database_config.migrate()
     directory_scanning.scanning()
