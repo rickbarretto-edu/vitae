4d67ac90622cd35836ca6acfa5cc73a3aefad964
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-10 19:56:38 -0300

    turn class into a function to reduce boilerplate

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,8 @@
 from src.database.database_config import database_config
 from src.database.database_creation import new_database
-from src.pipeline_etl.extract.dir_scanning import directory_scanning
+from src.pipeline_etl.extract.dir_scanning import scan_directory
 
 if __name__ == "__main__":
     new_database()
     database_config.migrate()
-    directory_scanning.scanning()
+    scan_directory()
--- src/pipeline_etl/extract/dir_scanning.py
+++ src/pipeline_etl/extract/dir_scanning.py
@@ -7,90 +7,81 @@ from src.utils.loggers import ConfigLogger
 configLogger = ConfigLogger(__name__)
 logger = configLogger.logger
 
+__all__ = ["scan_directory"]
 
-class DirectoryScanning:
+
+def scan_directory():
     """
-    Class to scan the directory of Lattes curricula and search for all zipped curricula.
+    SCANS THE DIRECTORY OF LATTES CURRICULA AND SEARCHES FOR ALL ZIPPED CURRICULA
     """
+    try:
+        current_directory = os.getcwd()
+        logger.info("Current directory: %s", current_directory)
 
-    def __init__(self):
-        pass
-
-    def scanning(self):
-        """
-        SCANS THE DIRECTORY OF LATTES CURRICULA AND SEARCHES FOR ALL ZIPPED CURRICULA
-        """
-        try:
-            current_directory = os.getcwd()
-            logger.info("Current directory: %s", current_directory)
-
-            curriculum_directory = os.path.join(current_directory, "repo")
-            if not os.path.exists(curriculum_directory):
-                logger.error(
-                    "Curriculum directory does not exist: %s", curriculum_directory
-                )
-                return
-
-            subdirectory_list = [
-                os.path.join(curriculum_directory, sub)
-                for sub in os.listdir(curriculum_directory)
-                if os.path.isdir(os.path.join(curriculum_directory, sub))
-            ]
-
-            with ThreadPoolExecutor(max_workers=8) as executor:
-                for subdirectory_path in subdirectory_list:
-                    executor.submit(self.process_subdir, subdirectory_path)
-
-            logger.info("Complete scan of all subdirectories.")
-        except Exception as e:
-            logger.error("An error occurred during the scan: %s", e)
-
-
-    def process_subdir(self, subdirectory):
-        """
-        Processes a single subdirectory containing curricula files.
-        """
-        if not os.path.exists(subdirectory):
-            logger.warning("Subdirectory does not exist: %s", subdirectory)
+        curriculum_directory = os.path.join(current_directory, "repo")
+        if not os.path.exists(curriculum_directory):
+            logger.error(
+                "Curriculum directory does not exist: %s", curriculum_directory
+            )
             return
 
-        curricula = os.listdir(subdirectory)
+        subdirectory_list = [
+            os.path.join(curriculum_directory, sub)
+            for sub in os.listdir(curriculum_directory)
+            if os.path.isdir(os.path.join(curriculum_directory, sub))
+        ]
 
-        logger.info(
-            "Processing subdirectory: %s with %d curricula", subdirectory, len(curricula)
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            for subdirectory_path in subdirectory_list:
+                executor.submit(process_subdir, subdirectory_path)
+
+        logger.info("Complete scan of all subdirectories.")
+    except Exception as e:
+        logger.error("An error occurred during the scan: %s", e)
+
+
+def process_subdir(subdirectory):
+    """
+    Processes a single subdirectory containing curricula files.
+    """
+    if not os.path.exists(subdirectory):
+        logger.warning("Subdirectory does not exist: %s", subdirectory)
+        return
+
+    curricula = os.listdir(subdirectory)
+
+    logger.info(
+        "Processing subdirectory: %s with %d curricula", subdirectory, len(curricula)
+    )
+
+    general_data_buffer = []
+    profession_buffer = []
+    research_area_buffer = []
+    education_buffer = []
+
+    flush = False
+    count = 0
+    for curriculum in curricula:
+        curriculum_path = os.path.join(subdirectory, curriculum)
+        logger.debug("Opening curriculum: %s", curriculum_path)
+
+        if count == 50:
+            flush = True
+            logger.info("Flushing buffers to database.")
+
+        parser.open_curriculum(
+            curriculum_path,
+            general_data_buffer,
+            profession_buffer,
+            research_area_buffer,
+            education_buffer,
+            flush,
         )
+        count += 1
 
-        general_data_buffer = []
-        profession_buffer = []
-        research_area_buffer = []
-        education_buffer = []
+    logger.info("Subdirectory %s processed successfully.", subdirectory)
 
-        flush = False
-        count = 0
-        for curriculum in curricula:
-            curriculum_path = os.path.join(subdirectory, curriculum)
-            logger.debug("Opening curriculum: %s", curriculum_path)
-
-            if count == 50:
-                flush = True
-                logger.info("Flushing buffers to database.")
-
-            parser.open_curriculum(
-                curriculum_path,
-                general_data_buffer,
-                profession_buffer,
-                research_area_buffer,
-                education_buffer,
-                flush,
-            )
-            count += 1
-
-        logger.info("Subdirectory %s processed successfully.", subdirectory)
-
-        general_data_buffer.clear()
-        profession_buffer.clear()
-        research_area_buffer.clear()
-        education_buffer.clear()
-
-
-directory_scanning = DirectoryScanning()
+    general_data_buffer.clear()
+    profession_buffer.clear()
+    research_area_buffer.clear()
+    education_buffer.clear()
