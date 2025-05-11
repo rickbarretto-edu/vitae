37c101a1c51f16222d5723ca929e6886a389f5fd
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-11 11:09:17 -0300

    use vitae instead of .env

--- src/database/database_config.py
+++ src/database/database_config.py
@@ -6,13 +6,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from src.utils.load_env import load_env
+from src.utils.settings import VitaeSettings, vitae
 
+__all__ = ["database_config"]
 
 class DatabaseConfig:
-    def __init__(self):
-        self.url = self.postgres_url
-        self.engine = create_engine(self.postgres_url)
+    def __init__(self, vitae: VitaeSettings):
+        self.engine = create_engine(vitae.postgres.url)
 
         self.session_local = sessionmaker(
             autocommit=False, autoflush=False, bind=self.engine
@@ -25,20 +25,5 @@ class DatabaseConfig:
 
         command.upgrade(Config(os.path.join(current_dir, ini_file)), "head")
 
-    @property
-    def postgres_url(self) -> str:
-        """Postgres URL from .env file"""
 
-        host = load_env.database_host
-        port = load_env.database_port
-        database = load_env.database_name
-
-        user = load_env.database_user
-        password = load_env.database_password
-
-        return (
-            f"postgresql+psycopg://{user}:{password}@{host}:{port}/{database}"
-        )
-
-
-database_config = DatabaseConfig()
+database_config = DatabaseConfig(vitae)
