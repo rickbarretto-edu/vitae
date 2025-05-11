2575418b2d8117ef454bab2e40f8466f7e6f1e1f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-11 11:24:35 -0300

    simplify class using dataclass

--- src/database/database_config.py
+++ src/database/database_config.py
@@ -1,24 +1,24 @@
+from dataclasses import dataclass
 import os
 
 from alembic import command
 from alembic.config import Config
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from src.utils.settings import VitaeSettings, vitae
+from src.utils.settings import vitae
 
 __all__ = ["Model", "database_config"]
 
 Model = declarative_base()
 
+@dataclass
 class DatabaseConfig:
-    def __init__(self, vitae: VitaeSettings):
-        self.engine = create_engine(vitae.postgres.url)
-
-        self.session_local = sessionmaker(
-            autocommit=False, autoflush=False, bind=self.engine
-        )
+    engine: Engine = create_engine(vitae.postgres.url)
+    session_local = sessionmaker(
+        autocommit=False, autoflush=False, bind=engine
+    )
 
     def migrate(self):
         current_dir: str = os.path.dirname(__file__)
@@ -27,4 +27,4 @@ class DatabaseConfig:
         command.upgrade(Config(os.path.join(current_dir, ini_file)), "head")
 
 
-database_config = DatabaseConfig(vitae)
+database_config = DatabaseConfig()
