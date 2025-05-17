6acabc6cc44d5f6101a1bda100c94d4efac71d27
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-17 20:23:27 -0300

    export all models

---
+++ src/models/__init__.py
@@ -0,0 +1,5 @@
+from src.models.academic_background import AcademicBackground
+from src.models.knowledge_area import KnowledgeArea
+from src.models.professional_experience import ProfessionalExperience
+from src.models.research_area import ResearchArea
+from src.models.researcher import Researcher
