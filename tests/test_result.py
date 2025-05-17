1cec29319bf077b4d9c7c468664c8be1c1a8dfda
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-17 14:16:17 -0300

    add Result  test

---
+++ tests/test_result.py
@@ -0,0 +1,70 @@
+import pytest
+from src.utils.result import Err, Ok, Panic, Result, Some, catch
+
+
+class DescribeOk:
+    def has_value(self):
+        result: Result[str, str] = Ok("I'm here")
+
+        assert result
+        assert "I'm here" == result.value
+        assert "I'm here" == result.expected("Be there")
+
+    def has_no_error(self):
+        result: Result[str, str] = Ok("I'm here")
+
+        assert result.error is None
+
+    def it_should_be_some(self):
+        result: Result[str, str] = Ok("I'm here")
+
+        assert Some("I'm here") == result.as_either
+
+
+class DescribeErr:
+    def has_no_value(self):
+        result: Result[str, str] = Err("I have no value")
+
+        assert not result
+        assert result.value is None
+
+    def should_raise_to_expected(self):
+        result: Result[str, str] = Err("I have no value")
+
+        with pytest.raises(Panic) as panic:
+            result.expected("Have no value")
+
+        assert "Have no value" == str(panic.value)
+
+    def has_error(self):
+        result: Result[str, str] = Err("I'm an error")
+
+        assert "I'm an error" == result.error
+
+
+class TestResult:
+    def test_ok_variant(self):
+        result: Result[str, str] = Ok("Success")
+
+        assert isinstance(result, Ok)
+        assert not isinstance(result, Err)
+
+    def test_err_variant(self):
+        result: Result[str, str] = Err("Fail")
+
+        assert isinstance(result, Err)
+        assert not isinstance(result, Ok)
+
+
+class DescribeCatch:
+    def should_be_ok_when_not_raise(self):
+        result = catch(lambda: 1 // 1)
+
+        assert result
+        assert 1 == result.value
+
+    def should_be_err_when_raise(self):
+        result = catch(lambda: 1 // 0)
+
+        assert not result
+        assert isinstance(result.error, ZeroDivisionError)
