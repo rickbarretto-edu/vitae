4e7673f37eb6a94cd22ba091eaa81f2589e82de8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-17 18:54:57 -0300

    implement helper functions for function concatenation

---
+++ src/utils/functions.py
@@ -0,0 +1,46 @@
+from typing import Callable, Any
+
+
+def also(*actions: Callable[[Any], None]) -> Callable[[Any], Any]:
+    """Merge independent functions into a single one.
+
+    Example
+    -------
+
+        Buffer(
+            on_flush=piped(process_data, also(print, log)),
+            max=64
+        )
+    """
+
+    def function(data: Any) -> Any:
+        for action in actions:
+            action(data)
+        return data
+
+    return function
+
+
+def piped(*actions: Callable[[Any], Any]) -> Callable[[Any], Any]:
+    """Pipe dependent functions into a single one.
+
+    Note
+    ----
+    The output of the latest function must be the same type
+    of the input of the next one.
+
+    Example
+    -------
+
+        Buffer(
+            on_flush=piped(process_data, also(print, log)),
+            max=64
+        )
+    """
+
+    def function(data: Any) -> Any:
+        for action in actions:
+            data = action(data)
+        return data
+
+    return function
