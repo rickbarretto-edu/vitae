6d1e2d348ddd6eb0db752035aa10397fb3d53e06
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-18 13:22:19 -0300

    turn method into separate function

--- src/processing/parsing/__init__.py
+++ src/processing/parsing/__init__.py
@@ -7,11 +7,12 @@ from loguru import logger
 
 from src.processing.buffers import CurriculaBuffer
 from src.processing.parsing import xml
+from src.processing.parsing.academic_background import academic_background
 from src.processing.parsing.general_data import general_data
 from src.processing.parsing.professional_experiences import (
     professional_experiences,
 )
-from src.processing.parsing.academic_background import academic_background
+from src.processing.parsing.research_area import research_area
 from src.processing.parsing.logging import log_parsing
 
 
@@ -74,64 +75,8 @@ class CurriculumParser:
         for background in academic_background(self.id, self.data):
             self.buffers.educations.push(background)
 
-        for area in self.research_area():
+        for area in research_area(self.id, self.data):
             self.buffers.research_areas.push(area)
-
-    @log_parsing("Research Area")
-    @eliot.log_call(action_type="parsing")
-    def research_area(self) -> list[Any]:
-        """Extract research areas from the Lattes curriculum XML.
-
-        This function navigates through the XML structure of a Lattes curriculum
-        to extract information about research areas, including major knowledge
-        areas, knowledge areas, sub-knowledge areas, and specialties.
-
-        Parameters
-        ----------
-        curriculum : xml.etree.ElementTree.Element
-            The Lattes curriculum XML element.
-
-        Returns
-        -------
-        list of dict
-            A list of dictionaries, where each dictionary contains information
-            about a research area with the following keys:
-            - 'major_knowledge_area' (str or None): The name of the major knowledge area.
-            - 'knowledge_area' (str or None): The name of the knowledge area.
-            - 'sub_knowledge_area' (str or None): The name of the sub-knowledge area.
-            - 'specialty' (str or None): The name of the specialty.
-
-        Notes
-        -----
-        If no research areas are found, an empty list is returned. In case of an
-        error during extraction, the function logs the error and returns an empty list.
-
-        Examples
-        --------
-        >>> curriculum = ET.parse("lattes.xml").getroot()
-        >>> parser = LattesParser()
-        >>> research_areas = parser.research_area(curriculum)
-        >>> print(research_areas)
-        [{'major_knowledge_area': 'Engineering', 'knowledge_area': 'Civil Engineering',
-          'sub_knowledge_area': 'Structural Engineering', 'specialty': 'Concrete Structures'}, ...]
-        """
-
-        return [
-            {
-                "researcher_id": self.id,
-                "major_knowledge_area": xml.attribute(
-                    area, "nome grande area do conhecimento"
-                ),
-                "knowledge_area": xml.attribute(
-                    area, "nome da area do conhecimento"
-                ),
-                "sub_knowledge_area": xml.attribute(
-                    area, "nome da sub-area do conhecimento"
-                ),
-                "specialty": xml.attribute(area, "nome da especialidade"),
-            }
-            for area in xml.find(self.data.element, "areas de atuacao") or []
-        ]
 
     # TODO Ajustar Areas de Conhecimento
     @log_parsing("Academic Background")
