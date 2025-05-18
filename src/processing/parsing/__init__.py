4e1f4e2b4c2804b004d6431261bf4a1397f5ebc8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-18 12:38:46 -0300

    move helper functions to xml module

--- src/processing/parsing/__init__.py
+++ src/processing/parsing/__init__.py
@@ -7,6 +7,13 @@ import eliot
 from loguru import logger
 
 from src.processing.parsing.logging import log_parsing
+from src.processing.parsing.xml import (
+    Node,
+    find,
+    attribute,
+    as_int,
+)
+
 from src.processing.buffers import CurriculaBuffer
 
 # TODO: those smaller functions should be moved to another module
@@ -17,53 +24,6 @@ from src.processing.buffers import CurriculaBuffer
 
 
 __all__ = ["CurriculumParser"]
-
-
-def normalized(tag: str) -> str:
-    return tag.upper().replace(" ", "-")
-
-
-def find(element: ET.Element | None, tag: str) -> ET.Element | None:
-    if element is None:
-        return None
-
-    return element.find(normalized(tag))
-
-
-def attribute(element: ET.Element | None, tag: str) -> str | None:
-    if element is None:
-        return None
-
-    if element_attribute := element.attrib.get(normalized(tag)):
-        return element_attribute.strip()
-
-
-def as_int(text: str | None) -> int | None:
-    if text is None:
-        return None
-
-    if text.isdigit():
-        return int(text)
-
-
-class Node:
-    def __init__(self, element: ET.Element | None):
-        self.element = element
-
-    def __getitem__(self, tag: str) -> str | None:
-        return attribute(self.element, tag)
-
-    @property
-    def exists(self) -> bool:
-        return self.element is not None
-
-    def first(self, tag: str) -> "Node":
-        return Node(find(self.element, tag))
-
-    def all(self, tag: str) -> list["Node"]:
-        if self.element is None:
-            return []
-        return [Node(e) for e in self.element.findall(normalized(tag))]
 
 
 # TODO: This should not be called from open_curriculum, but instanciated.
