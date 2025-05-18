18acd6a8cba77133ed4d8de78431ec05613c9678
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-18 20:49:48 -0300

    add explicit export

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,12 @@ from src.models.knowledge_area import KnowledgeArea
 from src.models.professional_experience import ProfessionalExperience
 from src.models.research_area import ResearchArea
 from src.models.researcher import Researcher
+
+
+__all__ = [
+    "AcademicBackground",
+    "KnowledgeArea",
+    "ProfessionalExperience",
+    "ResearchArea",
+    "Researcher",
+]
--- src/processing/parsing/parsers/__init__.py
+++ src/processing/parsing/parsers/__init__.py
@@ -3,3 +3,11 @@ from .general_data import general_data
 from .knowledge_areas import knowledge_areas
 from .professional_experiences import professional_experiences
 from .research_area import research_area
+
+__all__ = [
+    "academic_background",
+    "general_data",
+    "knowledge_areas",
+    "professional_experiences",
+    "research_area",
+]
