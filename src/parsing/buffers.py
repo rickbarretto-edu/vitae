a2365babc89eb44ba9996887439617f281a893b9
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-18 00:17:47 -0300

    create Buffers base

---
+++ src/parsing/buffers.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import Callable
+
+from src.utils.buffer import Buffer
+
+
+@dataclass(kw_only=True)
+class CurriculaBuffer:
+    general: Buffer
+    professions: Buffer
+    research_areas: Buffer
+    educations: Buffer
