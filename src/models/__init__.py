bfe295539b3eaa7393698ede5912645973d479ce
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-19 20:37:53 -0300

    only expose Researcher

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,14 +1,14 @@
-from src.models.academic_background import AcademicBackground
-from src.models.knowledge_area import KnowledgeArea
-from src.models.professional_experience import ProfessionalExperience
-from src.models.research_area import ResearchArea
+# from src.models.academic_background import AcademicBackground
+# from src.models.knowledge_area import KnowledgeArea
+# from src.models.professional_experience import ProfessionalExperience
+# from src.models.research_area import ResearchArea
 from src.models.researcher import Researcher
 
 
 __all__ = [
-    "AcademicBackground",
-    "KnowledgeArea",
-    "ProfessionalExperience",
-    "ResearchArea",
+    # "AcademicBackground",
+    # "KnowledgeArea",
+    # "ProfessionalExperience",
+    # "ResearchArea",
     "Researcher",
 ]
