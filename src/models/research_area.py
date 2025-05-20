6edb26a2bf0bae1745495180abbf4b280191d693
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-20 09:27:51 -0300

    temporarially comment code

--- src/models/research_area.py
+++ src/models/research_area.py
@@ -1,31 +1,31 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
-from sqlalchemy.orm import relationship
+# from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
+# from sqlalchemy.orm import relationship
 
-from src.models.__core__ import Model
-
-
-__all__ = ["ResearchArea"]
+# from src.models.__core__ import Model
 
 
-class ResearchArea(Model):
-    __tablename__ = "research_area"
+# __all__ = ["ResearchArea"]
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    major_knowledge_area = Column(String, nullable=False)
-    knowledge_area = Column(String, nullable=True)
-    sub_knowledge_area = Column(String, nullable=True)
-    specialty = Column(String, nullable=True)
-    researcher_id = Column(String, ForeignKey("researcher.id"), nullable=False)
 
-    researcher = relationship("Researcher", back_populates="research_area")
+# class ResearchArea(Model):
+#     __tablename__ = "research_area"
 
-    __table_args__ = (
-        UniqueConstraint(
-            "major_knowledge_area",
-            "knowledge_area",
-            "sub_knowledge_area",
-            "specialty",
-            "researcher_id",
-            name="unique_research_area",
-        ),
-    )
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     major_knowledge_area = Column(String, nullable=False)
+#     knowledge_area = Column(String, nullable=True)
+#     sub_knowledge_area = Column(String, nullable=True)
+#     specialty = Column(String, nullable=True)
+#     researcher_id = Column(String, ForeignKey("researcher.id"), nullable=False)
+
+#     researcher = relationship("Researcher", back_populates="research_area")
+
+#     __table_args__ = (
+#         UniqueConstraint(
+#             "major_knowledge_area",
+#             "knowledge_area",
+#             "sub_knowledge_area",
+#             "specialty",
+#             "researcher_id",
+#             name="unique_research_area",
+#         ),
+#     )
