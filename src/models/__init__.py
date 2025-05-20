a194bedb47fe07e2e1a8201316d5edf6095a3e70
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-20 08:43:01 -0300

    merge files and fix PK

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,14 +1,45 @@
-# from src.models.academic_background import AcademicBackground
-# from src.models.knowledge_area import KnowledgeArea
-# from src.models.professional_experience import ProfessionalExperience
-# from src.models.research_area import ResearchArea
-from src.models.researcher import Researcher
+from sqlmodel import SQLModel, Field, Relationship
 
 
-__all__ = [
-    # "AcademicBackground",
-    # "KnowledgeArea",
-    # "ProfessionalExperience",
-    # "ResearchArea",
-    "Researcher",
-]
+class Researcher(SQLModel, table=True):
+    id: str = Field(primary_key=True, nullable=False)
+    # updated_at: str | None = Field(
+    #     default=None, sa_column_kwargs={"default": "now()", "onupdate": "now()"}
+    # )
+    name: str = Field(nullable=False, index=True)
+    city: str | None = None
+    state: str | None = None
+    country: str | None = None
+    quotes_names: str | None = None
+    orcid: str | None = None
+    abstract: str | None = None
+    # professional_institution: str | None = None
+    # institution_state: str | None = None
+    # institution_city: str | None = None
+
+    # academic_background: list[AcademicBackground] = Relationship(
+    #     back_populates="researcher",
+    #     sa_relationship_kwargs={"cascade": "all, delete-orphan"},
+    # )
+    # professional_experience: list[ProfessionalExperience] = Relationship(
+    #     back_populates="researcher",
+    #     sa_relationship_kwargs={"cascade": "all, delete-orphan"},
+    # )
+    # research_area: list[ResearchArea] = Relationship(
+    #     back_populates="researcher",
+    #     sa_relationship_kwargs={"cascade": "all, delete-orphan"},
+    # )
+
+
+class ProfessionalExperience(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    institution: str = Field(nullable=False, index=True)
+    employment_relationship: str | None = None
+    start_year: int | None = None
+    end_year: int | None = None
+
+    researcher_id: str = Field(foreign_key="researcher.id", nullable=False)
+
+    # researcher: Researcher | None = Relationship(
+    #     back_populates="professional_experience"
+    # )
