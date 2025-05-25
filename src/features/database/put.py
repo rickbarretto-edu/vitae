e7974371052998045387ce329883404c4cff79ae
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-24 22:12:17 -0300

    make database independent from ingestion schema

---
+++ src/features/database/put.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Iterable
+
+from loguru import logger
+
+from sqlmodel import Session
+from sqlalchemy.engine import Engine
+
+from src import models
+
+
+@dataclass
+class PutOperations:
+    engine: Engine
+
+    def researchers(self, researchers: Iterable[models.Researcher]):
+        with Session(self.engine) as session:
+            for researcher in researchers:
+                session.add(researcher)
+                logger.debug("Researcher upserted: {}", researcher)
+            session.commit()
+
+    def experiences(
+        self, experiences: Iterable[models.ProfessionalExperience]
+    ):
+        with Session(self.engine) as session:
+            for experience in experiences:
+                session.add(experience)
+                logger.debug("Experience upserted: {}", experience)
+            session.commit()
+
+
+@dataclass
+class Database:
+    engine: Engine
+
+    @property
+    def put(self) -> PutOperations:
+        return PutOperations(self.engine)
