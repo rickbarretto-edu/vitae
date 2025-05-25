72ef24f1fffce45995cba1568d7ae064da089978
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-05-24 21:08:18 -0300

    put all models into a single module

--- src/models/__core__.py
+++
@@ -1,4 +0,0 @@
-
-from sqlalchemy.ext.declarative import declarative_base
-
-Model = declarative_base()
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field, Relationship, UniqueConstraint
 
 
 class Researcher(SQLModel, table=True):
@@ -17,21 +17,23 @@ class Researcher(SQLModel, table=True):
     institution_state: str | None = None
     institution_city: str | None = None
 
-    # academic_background: list[AcademicBackground] = Relationship(
-    #     back_populates="researcher",
-    #     sa_relationship_kwargs={"cascade": "all, delete-orphan"},
-    # )
-    # professional_experience: list[ProfessionalExperience] = Relationship(
-    #     back_populates="researcher",
-    #     sa_relationship_kwargs={"cascade": "all, delete-orphan"},
-    # )
-    # research_area: list[ResearchArea] = Relationship(
-    #     back_populates="researcher",
-    #     sa_relationship_kwargs={"cascade": "all, delete-orphan"},
-    # )
+    academic_background: list["AcademicBackground"] = Relationship(
+        back_populates="researcher",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"},
+    )
+    professional_experience: list["ProfessionalExperience"] = Relationship(
+        back_populates="researcher",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"},
+    )
+    research_area: list["ResearchArea"] = Relationship(
+        back_populates="researcher",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"},
+    )
 
 
 class ProfessionalExperience(SQLModel, table=True):
+    __tablename__: str = "professional_experience"
+
     id: int | None = Field(default=None, primary_key=True)
     institution: str = Field(nullable=False, index=True)
     employment_relationship: str | None = None
@@ -39,7 +41,78 @@ class ProfessionalExperience(SQLModel, table=True):
     end_year: int | None = None
 
     researcher_id: str = Field(foreign_key="researcher.id", nullable=False)
+    researcher: Researcher | None = Relationship(
+        back_populates="professional_experience"
+    )
 
-    # researcher: Researcher | None = Relationship(
-    #     back_populates="professional_experience"
-    # )
+
+class AcademicBackground(SQLModel, table=True):
+    __tablename__: str = "academic_background"
+
+    id: int | None = Field(default=None, primary_key=True)
+    type: str
+    institution: str
+    course: str | None = None
+    start_year: int | None = None
+    end_year: int | None = None
+    researcher_id: str = Field(foreign_key="researcher.id")
+
+    researcher: "Researcher" = Relationship(back_populates="academic_background")
+    knowledge_area: list["KnowledgeArea"] = Relationship(
+        back_populates="academic_background",
+        sa_relationship_kwargs={"cascade": "all, delete-orphan"},
+    )
+
+    __table_args__ = (
+        UniqueConstraint(
+            "type", "institution", "course", "start_year", "end_year", "researcher_id",
+            name="unique_academic_background"
+        ),
+        )
+
+class KnowledgeArea(SQLModel, table=True):
+    __tablename__: str = "knowledge_area"
+
+    id: int | None = Field(default=None, primary_key=True)
+    major_knowledge_area: str
+    knowledge_area: str | None = None
+    sub_knowledge_area: str | None = None
+    specialty: str | None = None
+    academic_background_id: int = Field(foreign_key="academic_background.id")
+
+    academic_background: "AcademicBackground" = Relationship(back_populates="knowledge_area")
+
+    __table_args__ = (
+        UniqueConstraint(
+            "major_knowledge_area",
+            "knowledge_area",
+            "sub_knowledge_area",
+            "specialty",
+            "academic_background_id",
+            name="unique_knowledge_area",
+        ),
+    )
+
+
+class ResearchArea(SQLModel, table=True):
+    __tablename__: str = "research_area"
+
+    id: int | None = Field(default=None, primary_key=True)
+    major_knowledge_area: str
+    knowledge_area: str | None = None
+    sub_knowledge_area: str | None = None
+    specialty: str | None = None
+    researcher_id: str = Field(foreign_key="researcher.id")
+
+    researcher: "Researcher" = Relationship(back_populates="research_area")
+
+    __table_args__ = (
+        UniqueConstraint(
+            "major_knowledge_area",
+            "knowledge_area",
+            "sub_knowledge_area",
+            "specialty",
+            "researcher_id",
+            name="unique_research_area",
+        ),
+    )
