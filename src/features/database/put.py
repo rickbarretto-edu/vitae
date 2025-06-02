766a274d4f2aeb78ec61cd256aaea15b5fc4ca42
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-02 00:01:48 -0300

    document module

--- src/features/database/put.py
+++ src/features/database/put.py
@@ -1,7 +1,8 @@
+"""Put database operations."""
+
 from collections.abc import Iterable
 from dataclasses import dataclass
 
-from loguru import logger
 from sqlalchemy.engine import Engine
 from sqlmodel import Session
 
@@ -10,15 +11,19 @@ from src import models
 
 @dataclass
 class PutOperations:
+    """Operations that puts data on the database."""
+
     engine: Engine
 
     def researchers(self, researchers: Iterable[models.Researcher]) -> None:
+        """Insert multiple researchers."""
         with Session(self.engine) as session:
             for researcher in researchers:
                 session.add(researcher)
                 session.commit()
 
     def researcher(self, researcher: models.Researcher) -> None:
+        """Insert a researcher."""
         with Session(self.engine) as session:
             session.add(researcher)
             session.commit()
@@ -27,12 +32,14 @@ class PutOperations:
         self,
         experiences: Iterable[models.ProfessionalExperience],
     ) -> None:
+        """Insert multiple Researcher's Professional Experience."""
         with Session(self.engine) as session:
             for experience in experiences:
                 session.add(experience)
                 session.commit()
 
     def experience(self, experience: models.ProfessionalExperience) -> None:
+        """Insert a Researcher's Professional Experience."""
         with Session(self.engine) as session:
             session.add(experience)
             session.commit()
@@ -41,11 +48,13 @@ class PutOperations:
         self,
         background: models.AcademicBackground,
     ) -> None:
+        """Insert a Researcher's Academic Background."""
         with Session(self.engine) as session:
             session.add(background)
             session.commit()
 
     def research_area(self, area: models.ResearchArea) -> None:
+        """Insert a Researcher's Area of Research."""
         with Session(self.engine) as session:
             session.add(area)
             session.commit()
