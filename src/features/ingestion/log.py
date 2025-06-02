6bc10618199717fedea779cd4a75cfdd1b521d03
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-02 00:25:01 -0300

    add module documentation

--- src/features/ingestion/log.py
+++ src/features/ingestion/log.py
@@ -1,3 +1,5 @@
+"""Ingestion's Log utils."""
+
 from pathlib import Path
 from typing import TypedDict
 
@@ -8,6 +10,13 @@ LOG_DIVIDER = "-" * 80
 
 
 def log_into[T: TypedDict | dict](data: T, log: Path) -> T:
+    """Log ``data`` into ``log``.
+
+    Returns
+    -------
+    The ``data`` itself
+
+    """
     with log.open("+a", encoding="utf-8") as file:
         for key, val in data.items():
             print(f"{key}: {val}", file=file)
