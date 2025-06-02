8fa286c5be3601560b49401dbb73c8eae47436fe
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-01 21:24:35 -0300

    refaactor function to use generators instead of list

--- src/features/ingestion/parser/academic_background.py
+++ src/features/ingestion/parser/academic_background.py
@@ -1,32 +1,44 @@
+from typing import Iterator
+
 import eliot
 
 from src.features.ingestion.schema import AcademicBackground
+
 from . import _xml as xml
-from ._log import log_parsing
 
 __all__ = ["academic_background"]
 
 
 @eliot.log_call(action_type="parsing")
-def academic_background(id: str, data: xml.Node) -> list[AcademicBackground]:
+def academic_background(
+    researcher_id: str,
+    data: xml.Node,
+) -> Iterator[AcademicBackground]:
     """Extract academic background information from a Lattes curriculum XML.
 
-    This function navigates through the XML tags of a Lattes curriculum
-    to extract information about the academic background of a researcher.
+    Academic Backgound is a section of general data.
+    This contains all the Researcher's academic data such as courses
+    and graduations.
 
-    Returns
-    -------
+    Yields
+    ------
     Researcher's Academic backgrounds.
 
+    XML Schema
+    ----------
+        formacao-academica-titulacao: [
+            
+        ]
+
     """
-    return [
-        AcademicBackground(
-            researcher_id=id,
-            type=bg.tag,
-            institution=xml.attribute(bg, "nome instituicao"),
-            course=xml.attribute(bg, "nome curso"),
-            start_year=xml.as_int(xml.attribute(bg, "ano de inicio")),
-            end_year=xml.as_int(xml.attribute(bg, "ano de conclusao")),
+    for background in (
+        xml.find(data.element, "formacao academica titulacao") or []
+    ):
+        yield AcademicBackground(
+            researcher_id=researcher_id,
+            type=background.tag,
+            institution=xml.attribute(background, "nome instituicao"),
+            course=xml.attribute(background, "nome curso"),
+            start_year=xml.as_int(xml.attribute(background, "ano de inicio")),
+            end_year=xml.as_int(xml.attribute(background, "ano de conclusao")),
         )
-        for bg in xml.find(data.element, "formacao academica titulacao") or []
-    ]
