18863ef5761002c1abd823c384077f8f1b574f00
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-02 00:23:12 -0300

    refactor variable into a constant and evaluate this once

--- src/features/ingestion/log.py
+++ src/features/ingestion/log.py
@@ -4,11 +4,14 @@ from typing import TypedDict
 
 __all__ = ["log_into"]
 
+LOG_DIVIDER = "-" * 80
+
 
 def log_into[T: TypedDict | dict](data: T, log: Path) -> T:
     with log.open("+a", encoding="utf-8") as file:
         for key, val in data.items():
             print(f"{key}: {val}", file=file)
-        print("-" * 80, file=file, flush=True)
+
+        print(LOG_DIVIDER, file=file, flush=True)
 
     return data
