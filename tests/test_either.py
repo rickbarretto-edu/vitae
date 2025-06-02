17838d6b6e448f6ae51d15b7ba05a01547868faa
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-02 14:58:05 -0300

    add missing type hints

--- tests/test_either.py
+++ tests/test_either.py
@@ -4,14 +4,14 @@ from src.lib.result import Either, Empty, Panic, Some
 
 
 class DescribeSome:
-    def its_default_is_none(self):
+    def its_default_is_none(self) -> None:
         result: Either = Some()
 
         assert result
         assert result.value is None
         assert result.expected("Be None") is None
 
-    def has_value(self):
+    def has_value(self) -> None:
         result: Either[str] = Some("Value")
 
         assert result
@@ -21,7 +21,7 @@ class DescribeSome:
 
 
 class DescribeEmpty:
-    def should_accept_useless_parameter(self):
+    def should_accept_useless_parameter(self) -> None:
         result: Either[str] = Empty("value")
 
         assert not result
@@ -31,13 +31,13 @@ class DescribeEmpty:
             result.expected("There is no value")
         assert str(panic.value) == "There is no value"
 
-    def has_no_value(self):
+    def has_no_value(self) -> None:
         result: Either[str] = Empty()
 
         assert not result
         assert result.value is None
 
-    def should_raise_to_expected(self):
+    def should_raise_to_expected(self) -> None:
         result: Either[str] = Empty()
 
         with pytest.raises(Panic) as panic:
@@ -47,17 +47,17 @@ class DescribeEmpty:
 
 
 class TestEither:
-    def test_some_variant(self):
+    def test_some_variant(self) -> None:
         success: Either[str] = Some("Success")
         assert isinstance(success, Some)
         assert not isinstance(success, Empty)
 
-    def test_empty_variant(self):
+    def test_empty_variant(self) -> None:
         failed: Either[str] = Empty()
         assert isinstance(failed, Empty)
         assert not isinstance(failed, Some)
 
-    def test_and(self):
+    def test_and(self) -> None:
         x, y = "x", "y"
 
         assert Empty() == (Empty() and Empty())
@@ -65,7 +65,7 @@ class TestEither:
         assert Empty() == (Some(x) and Empty())
         assert Some(y) == (Some(x) and Some(y))
 
-    def test_or(self):
+    def test_or(self) -> None:
         x, y = "x", "y"
 
         assert Empty() == (Empty() or Empty())
@@ -73,7 +73,7 @@ class TestEither:
         assert Some(x) == (Some(x) or Empty())
         assert Some(x) == (Some(x) or Some(y))
 
-    def test_xor(self):
+    def test_xor(self) -> None:
         x, y = "x", "y"
 
         assert Empty() == (Empty() ^ Empty())
--- tests/test_result.py
+++ tests/test_result.py
@@ -5,39 +5,39 @@ from tests.utils import should
 
 
 class DescribeOk:
-    def it_has_value(self):
+    def it_has_value(self) -> None:
         result: Result[str, str] = Ok("I'm here")
 
         assert result
         assert result.value == "I'm here"
         assert result.expected("Be there") == "I'm here"
 
-    def it_has_no_error(self):
+    def it_has_no_error(self) -> None:
         result: Result[str, str] = Ok("I'm here")
 
         assert result.error is None
 
     @should("be Some")
-    def when_as_either(self):
+    def when_as_either(self) -> None:
         result: Result[str, str] = Ok("I'm here")
 
         assert Some("I'm here") == result.as_either
 
 
 class DescribeErr:
-    def it_has_no_value(self):
+    def it_has_no_value(self) -> None:
         result: Result[str, str] = Err("I have no value")
 
         assert not result
         assert result.value is None
 
-    def it_has_error(self):
+    def it_has_error(self) -> None:
         result: Result[str, str] = Err("I'm an error")
 
         assert result.error == "I'm an error"
 
     @should("Panic")
-    def when_expected_value_is_not_there(self):
+    def when_expected_value_is_not_there(self) -> None:
         result: Result[str, str] = Err("I have no value")
 
         with pytest.raises(Panic) as panic:
@@ -48,14 +48,14 @@ class DescribeErr:
 
 class TestResult:
     @should("be instance of Ok")
-    def when_comes_from_ok(self):
+    def when_comes_from_ok(self) -> None:
         result: Result[str, str] = Ok("Success")
 
         assert isinstance(result, Ok)
         assert not isinstance(result, Err)
 
     @should("be instance of Err")
-    def when_comes_from_err(self):
+    def when_comes_from_err(self) -> None:
         result: Result[str, str] = Err("Fail")
 
         assert isinstance(result, Err)
@@ -64,20 +64,20 @@ class TestResult:
 
 class DescribeCatch:
     @should("return Ok")
-    def when_expression_not_raises(self):
+    def when_expression_not_raises(self) -> None:
         result: Result[int, ZeroDivisionError] = catch(lambda: 1 // 1)
 
         assert result
         assert result.value == 1
 
     @should("return Err")
-    def when_expression_raises(self):
+    def when_expression_raises(self) -> None:
         result: Result[int, ZeroDivisionError] = catch(lambda: 1 // 0)
 
         assert not result
         assert isinstance(result.error, ZeroDivisionError)
 
-    def test_and(self):
+    def test_and(self) -> None:
         e, d = "e", "d"
         x, y = "x", "y"
 
@@ -86,7 +86,7 @@ class DescribeCatch:
         assert Err(d) == (Ok(x) and Err(d))
         assert Ok(y) == (Ok(x) and Ok(y))
 
-    def test_or(self):
+    def test_or(self) -> None:
         e, d = "e", "d"
         x, y = "x", "y"
 
