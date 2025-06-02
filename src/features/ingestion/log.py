9d65c53fa729f6a2bf54cdbae580762afc6d9b3a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-02 00:21:39 -0300

    remove unused function

--- src/features/ingestion/log.py
+++ src/features/ingestion/log.py
@@ -1,44 +1,8 @@
-from __future__ import annotations
-
-from functools import wraps
 from pathlib import Path
-import pprint
-from typing import Any, TypedDict
-
-from loguru import logger
-
-from src.lib.result import Result, catch
-
-from .parser._xml import ParsingError
-
-__all__ = ["log_parsing"]
+from typing import TypedDict
 
 
-def log_parsing(topic: str):  # noqa: ANN202
-    def decorator(func):  # noqa: ANN202
-        @wraps(func)
-        def wrapper(*args, **kwargs):  # noqa: ANN202
-            logger.debug("Parsing {}'s data...", topic)
-
-            result: Result[list | dict, ParsingError] = catch(
-                lambda: func(*args, **kwargs),
-            )
-
-            if result:
-                logger.debug("{}'s data successfully extracted.", topic)
-                logger.trace("Parsed data:\n{}", pprint.pformat(result.value))
-                return result.value
-            logger.error(
-                "Error when extracting {}'s data...: {}",
-                topic,
-                str(result.error),
-            )
-            return_type = func.__annotations__.get("return")
-            return [] if isinstance(return_type, list) else {}
-
-        return wrapper
-
-    return decorator
+__all__ = ["log_into"]
 
 
 def log_into[T: TypedDict | dict](data: T, log: Path) -> T:
