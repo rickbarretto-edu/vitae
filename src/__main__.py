c7bb7a30f892387d72dba5f1e7570e87f680d391
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-02 18:00:29 -0300

    remove unnecessary code

--- src/__main__.py
+++ src/__main__.py
@@ -1,24 +1,16 @@
-from typing import TYPE_CHECKING
-
-from sqlmodel import create_engine
-
 from src import settings
 from src.__setup__ import setup_database, setup_vitae
 from src.features.database import Database
 from src.features.ingestion.scanner import CurriculaScheduler
 
-if TYPE_CHECKING:
-    from sqlalchemy import Engine
-
 
 def main() -> None:
     vitae: settings.VitaeSettings = settings.load()
-    engine: Engine = create_engine(vitae.postgres.url, echo=True)
 
     setup_vitae(vitae)
     setup_database(vitae)
 
-    database = Database(engine)
+    database = Database(vitae.postgres.engine)
     CurriculaScheduler(vitae, database).scan()
 
 
