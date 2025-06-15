3ed7985e6f7175c303abfc44320654323c6b117c
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-14 22:02:26 -0300

    Simplify all methods into a single one
    There is no need to all these methods.

--- src/infra/database/put.py
+++ src/infra/database/put.py
@@ -6,7 +6,7 @@ from collections.abc import Iterable
 from dataclasses import dataclass
 
 from sqlalchemy.engine import Engine
-from sqlmodel import SQLModel, Session
+from sqlmodel import Session
 
 from src.infra.database import schema
 
@@ -26,46 +26,9 @@ class PutOperations:
         | Iterable[schema.AcademicBackground],
         area: schema.ResearchArea | Iterable[schema.ResearchArea],
     ) -> None:
-        self.researcher(researcher)
-        self.experience(experience)
-        self.academic_background(background)
-        self.research_area(area)
-
-    def researcher(
-        self,
-        researcher: schema.Researcher | Iterable[schema.Researcher],
-    ) -> None:
-        """Insert one or more researchers."""
-        self._put(researcher)
-
-    def experience(
-        self,
-        experience: schema.ProfessionalExperience
-        | Iterable[schema.ProfessionalExperience],
-    ) -> None:
-        """Insert one or more Professional Experiences."""
-        self._put(experience)
-
-    def academic_background(
-        self,
-        background: schema.AcademicBackground
-        | Iterable[schema.AcademicBackground],
-    ) -> None:
-        """Insert one or more Academic Backgrounds."""
-        self._put(background)
-
-    def research_area(
-        self,
-        area: schema.ResearchArea | Iterable[schema.ResearchArea],
-    ) -> None:
-        """Insert one or more Research Areas."""
-        self._put(area)
-
-    def _put(self, data: SQLModel | Iterable[SQLModel]) -> None:
-        """Insert SQLModel(s) into the database and commit them."""
         with Session(self.engine) as session:
-            if isinstance(data, SQLModel):
-                session.add(data)
-            else:
-                session.add_all(list(data))
+            session.add(researcher)
+            session.add_all(experience)
+            session.add_all(background)
+            session.add_all(area)
             session.commit()
