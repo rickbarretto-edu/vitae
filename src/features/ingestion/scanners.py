2441fe0a582148dc58a62c71abbb53b72edc583f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-20 21:21:06 -0300

    Refactor inlined type into typealias

--- src/features/ingestion/scanners.py
+++ src/features/ingestion/scanners.py
@@ -6,24 +6,22 @@ from typing import Callable, Protocol
 
 __all__ = ["Scanner", "parallel", "serial"]
 
+
+type FileParser = Callable[[Path], None]
+
+
 class Scanner(Protocol):
-    def __call__(self, all_files: Path, action: Callable[[Path], None]) -> None:
+    def __call__(self, all_files: Path, action: FileParser) -> None:
         pass
 
 
-def serial(
-    all_files: Path,
-    action: Callable[[Path], None],
-) -> None:
+def serial(all_files: Path, action: FileParser) -> None:
     """Scan & Process files in order."""
     for directory in all_files.iterdir():
         action(directory)
 
 
-def parallel(
-    all_files: Path,
-    action: Callable[[Path], None],
-) -> None:
+def parallel(all_files: Path, action: FileParser) -> None:
     """Scan & Process files in parallel (I/O bound)."""
     with ThreadPoolExecutor(max_workers=8) as executor:
         for directory in all_files.iterdir():
