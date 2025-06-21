7c3a21a74b048cacfcbbd68912c2bc4f42a28814
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-20 21:25:37 -0300

    Document Protocol

--- src/features/ingestion/scanners.py
+++ src/features/ingestion/scanners.py
@@ -11,8 +11,22 @@ type FileParser = Callable[[Path], None]
 
 
 class Scanner(Protocol):
+    """Scanning Strategy Protocol.
+
+    All Scanning strategies must follow this Protocol.
+    """
+
     def __call__(self, all_files: Path, action: FileParser) -> None:
-        pass
+        """All scanners follows this pattern.
+
+        You can define your scanner as a raw function:
+
+        Example:
+        -------
+            def scanner(all_files: Path, action: FileParser) -> None:
+                ...
+
+        """
 
 
 def serial(all_files: Path, action: FileParser) -> None:
