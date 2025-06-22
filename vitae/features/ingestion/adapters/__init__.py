e19b625837abccf13fa4bfdfaf7d80c0ac928565
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-22 01:01:43 -0300

    Export all adapters

--- vitae/features/ingestion/adapters/__init__.py
+++ vitae/features/ingestion/adapters/__init__.py
@@ -1,3 +1,15 @@
-from .researcher import Researcher
+from .academic import Education, Institution, StudyField
+from .professional import Address, Business, Experience
+from .researcher import Expertise, Nationality, Researcher
 
-__all__ = ["Researcher"]
+__all__ = [
+    "Address",
+    "Business",
+    "Education",
+    "Experience",
+    "Expertise",
+    "Institution",
+    "Nationality",
+    "Researcher",
+    "StudyField",
+]
