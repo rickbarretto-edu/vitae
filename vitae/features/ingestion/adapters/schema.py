31a74ed3d744bb85d165f8ba9cff799fa62bc304
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-21 23:02:40 -0300

    Map to Database Schema from adapters

--- vitae/features/ingestion/adapters/schema.py
+++ vitae/features/ingestion/adapters/schema.py
@@ -2,21 +2,26 @@
 
 from __future__ import annotations
 
+from collections.abc import Iterable, Iterator
+from dataclasses import dataclass
 from typing import TypedDict
+
+from src.infra.database import schema as db
 
 __all__ = [
     "AcademicBackground",
     "Expertise",
-    "GeneralData",
     "Nationality",
     "ProfessionalExperience",
+    "Researcher",
 ]
 
 
 # =~=~=~=~=~=~ Researcher ~=~=~=~=~=~=
 
 
-class GeneralData(TypedDict):
+@dataclass
+class Researcher:
     """Researcher's personal and general data."""
 
     lattes_id: str
@@ -25,23 +30,62 @@ class GeneralData(TypedDict):
     orcid: str | None
     abstract: str | None
 
+    nationality: Nationality
+    expertise: Iterable[Expertise]
 
-class Nationality(TypedDict):
+    @property
+    def as_table(self) -> db.Researcher:
+        return db.Researcher(
+            lattes_id=self.lattes_id,
+            full_name=self.full_name,
+            quotes_names=self.quotes_names,
+            orcid=self.orcid,
+            abstract=self.abstract,
+        )
+
+    @property
+    def nationality_table(self) -> db.Nationality:
+        return self.nationality.as_table(self.lattes_id)
+
+    @property
+    def expertise_tables(self) -> Iterator[db.Expertise]:
+        return (
+            expertise.as_table(self.lattes_id) for expertise in self.expertise
+        )
+
+
+@dataclass
+class Nationality:
     """Researcher's Nationality."""
 
-    researcher_id: str
     born_country: str | None
     nationality: str | None
 
+    def as_table(self, researcher_id: str) -> db.Nationality:
+        db.Nationality(
+            researcher_id=researcher_id,
+            born_country=self.born_country,
+            nationality=self.nationality,
+        )
 
-class Expertise(TypedDict):
+
+@dataclass
+class Expertise:
     """Researcher's Expertise."""
 
-    researcher_id: str
     major: str | None
     area: str | None
     sub: str | None
     speciality: str | None
+
+    def as_table(self, researcher_id: str) -> db.Expertise:
+        return db.Expertise(
+            researcher_id=researcher_id,
+            major=self.major,
+            area=self.area,
+            sub=self.sub,
+            speciality=self.speciality,
+        )
 
 
 class AcademicBackground(TypedDict):
