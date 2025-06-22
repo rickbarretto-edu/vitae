cce7b580575d7e4428db5785e0d06f740bebefa5
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-21 22:18:17 -0300

    Remove boilerplate

--- vitae/features/ingestion/parsing/__init__.py
+++ vitae/features/ingestion/parsing/__init__.py
@@ -9,7 +9,7 @@ from .academic_background import academic_background
 from .general_data import expertise, general_data, nationality
 from .professional_experiences import professional_experiences
 
-__all__ = ["CurriculumParser"]
+__all__ = ["CurriculumParser", "schema"]
 
 
 class CurriculumParser:
@@ -30,30 +30,16 @@ class CurriculumParser:
 
     @property
     def all(self) -> domain.Curriculum:
+        researcher = general_data(self.id, self.data)
+        nationality_ = nationality(self.id, self.data)
+        expertise_ = expertise(self.id, self.data)
+        experiences = professional_experiences(self.id, self.data)
+        background = academic_background(self.id, self.data)
+
         return domain.Curriculum(
-            _personal_data=self.researcher,
-            _nationality=self.nationality,
-            _expertise=self.expertise,
-            _academic_background=self.background,
-            _professional_experiences=self.experiences,
+            _personal_data=researcher,
+            _nationality=nationality_,
+            _expertise=expertise_,
+            _academic_background=background,
+            _professional_experiences=experiences,
         )
-
-    @property
-    def researcher(self) -> schema.GeneralData:
-        return general_data(self.id, self.data)
-
-    @property
-    def nationality(self) -> schema.Nationality:
-        return nationality(self.id, self.data)
-
-    @property
-    def expertise(self) -> Iterator[schema.Expertise]:
-        yield from expertise(self.id, self.data)
-
-    @property
-    def experiences(self) -> Iterator[schema.ProfessionalExperience]:
-        yield from professional_experiences(self.id, self.data)
-
-    @property
-    def background(self) -> Iterator[schema.AcademicBackground]:
-        yield from academic_background(self.id, self.data)
