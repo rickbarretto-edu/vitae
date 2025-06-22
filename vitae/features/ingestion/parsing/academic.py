692e5048fd7ba0da57b48c5b4e685011031ee819
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-22 01:02:07 -0300

    Re-implement academic parser from scratch

--- vitae/features/ingestion/parsing/academic.py
+++ vitae/features/ingestion/parsing/academic.py
@@ -1,126 +1,59 @@
 from collections.abc import Iterator
+import uuid
 
-from vitae.features.ingestion.adapters.schema import AcademicBackground
+from vitae.features.ingestion.adapters import Education
+from vitae.features.ingestion.adapters.academic import Institution
 
 from . import _xml as xml
 
-__all__ = ["academic_background"]
+__all__ = ["education_from_xml"]
 
 
-def academic_background(
+def education_from_xml(
     researcher_id: str,
     data: xml.Node,
-) -> Iterator[AcademicBackground]:
-    """Extract academic background information from a Lattes curriculum XML.
-
-    Academic Backgound is a section of general data.
-    This contains all the Researcher's academic data such as courses
-    and graduations.
+) -> Iterator[Education]:
+    """Extract education information from a Lattes curriculum XML.
 
     Yields
     ------
-    Researcher's Academic backgrounds.
-
-    XML Schema
-    ----------
-        formacao-academica-titulacao: [
-            graduacao: {
-                sequencia-formacao: integer
-                nivel: integer
-                titulo-do-trabalho-de-conclusao-de-curso: string
-                nome-do-orientador: string
-                codigo-instituicao: string
-                nome-instituicao: string
-                codigo-orgao: string
-                nome-orgao: string
-                curso-codigo: string
-                nome-curso: string
-                codigo-area-curso: string
-                status-do-curso: string
-                ano-de-inicio: integer
-                ano-de-conclusao: integer
-                flag-bolsa: boolean
-                codigo-agencia-financiadora: string
-                nome-agencia: string
-                numero-id-orientador: string
-                codigo-curso-capes: string
-                formacao-academica-titulacao: string
-                ...
-            },
-            especializacao: {
-                sequencia-formacao: integer
-                nivel: integer
-                titulo-da-monografia: string
-                nome-do-orientador: string
-                codigo-instituicao: string
-                nome-instituicao: string
-                codigo-orgao: string
-                nome-orgao: string
-                codigo-curso: string
-                nome-curso: string
-                status-do-curso: string
-                ano-de-inicio: integer
-                ano-de-conclusao: integer
-                flag-bolsa: boolean
-                codigo-agencia-financiadora: string
-                nome-agencia: string
-                carga-horaria: integer
-            }
-
-            mestrado | doutorado: {
-                sequencia-formacao: integer
-                nivel: integer
-                codigo-instituicao: string
-                nome-instituicao: string
-                codigo-orgao: string
-                nome-orgao: string
-                codigo-curso: string
-                nome-curso: string
-                codigo-area-curso: string
-                status-do-curso: string
-                ano-de-inicio: integer
-                ano-de-conclusao: integer
-                flag-bolsa: boolean
-                codigo-agencia-financiadora: string
-                nome-agencia: string
-                ano-de-obtencao-do-titulo: integer
-                titulo-da-dissertacao-tese: string
-                nome-completo-orientador: string
-                tipo-mestrado: string
-                numero-id-orientador: string
-                codigo-curso-capes: string
-                nome-do-co-orientador: string
-
-                tipo-doutorado: string
-                codigo-instituicao-dout: string
-                nome-instituicao-dout: string
-                codigo-instituicao-outra-dout: string
-                nome-instituicao-outra-dout: string
-                nome-orientador-dout: string
-                nome-do-co-orientador: string
-                nome-do-orientador-sanduiche: string
-                nome-do-orientador-co-tutela: string
-                codigo-instituicao-co-tutela: string
-                codigo-instituicao-sanduiche: string
-                codigo-instituicao-outra-co-tutela: string
-                codigo-instituicao-outra-sanduiche: string
-                ...
-            }
-        ]
+    Researcher's Education Background.
 
     """
-    for background in (
-        xml.find(data.element, "formacao academica titulacao") or []
-    ):
-        institution = xml.attribute(background, "nome instituicao")
-        if institution is None:
-            institution = "Unknown Institution"
+    education_summary = data.first("formacao academica titulacao").element
 
-        yield AcademicBackground(
-            researcher_id=researcher_id,
-            type=background.tag,
-            institution=institution,
-            course=xml.attribute(background, "nome curso"),
-            start_year=xml.as_int(xml.attribute(background, "ano de inicio")),
-            end_year=xml.as_int(xml.attribute(background, "ano de conclusao")),
-        )
+    if education_summary is not None:
+        for edu in education_summary.iter():
+            education = xml.Node(edu)
+
+            yield Education(
+                id=uuid.uuid1(),
+                researcher_id=researcher_id,
+                category=edu.tag,
+                course=education["nome curso"],
+                start=xml.as_int(education["ano de inicio"]),
+                end=xml.as_int(education["ano de conclusao"]),
+                institution=institution(
+                    education,
+                    data.first("informacoes adicionais instituicoes"),
+                ),
+                fields=[],
+            )
+
+
+def institution(education: xml.Node, extra_data: xml.Node) -> Institution:
+    lattes_id: str = education["codigo instituicao"] or ""
+    found: xml.Node = xml.Node(None)
+
+    for inst in extra_data.all("informacao adicional instituicao"):
+        if inst["codigo instituicao"] == lattes_id:
+            found = inst
+            break
+
+    return Institution(
+        lattes_id=lattes_id,
+        name=education["nome instituicao"],
+        country=found["nome pais instituicao"],
+        state=found["sigla uf instituicao"],
+        abbr=found["sigla instituicao"],
+    )
