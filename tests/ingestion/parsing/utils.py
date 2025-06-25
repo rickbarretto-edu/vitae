bc40f6ac3eeaf1800b12fc94d1b1d493fc148c14
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-24 22:22:50 -0300

    Create utils for testing parsers

---
+++ tests/ingestion/parsing/utils.py
@@ -0,0 +1,37 @@
+
+import abc
+from xml.dom.minidom import parseString
+
+from vitae.features.ingestion.parsing._xml import Node, parse
+
+
+__all__ = [
+    "XmlString",
+    "Document"
+]
+
+type XmlString = str
+
+class Document(abc.ABC):
+
+    @property
+    @abc.abstractmethod
+    def template(self) -> XmlString:
+        ...
+
+    def __str__(self) -> str:
+        xml = f"""<?xml version="1.0" ?>
+        <CURRICULO-VITAE>
+            {self.template}
+        </CURRICULO-VITAE>
+        """
+        return self._formated(xml)
+    
+    @property
+    def as_node(self) -> Node:
+        return parse(str(self))
+    
+
+    def _formated(self, content: XmlString) -> XmlString:
+        return parseString(content).toprettyxml(indent="    ")
+        
