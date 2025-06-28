5ef16fa1aecee9cf28dee809c83724074f828386
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-06-28 12:49:35 -0300

    Export new Advisoring Table

--- vitae/infra/database/tables/__init__.py
+++ vitae/infra/database/tables/__init__.py
@@ -1,12 +1,13 @@
 from sqlmodel import SQLModel
 
-from .academic import Education, StudyField
+from .academic import Advisoring, Education, StudyField
 from .institution import Institution
 from .professional import Address, Experience
 from .researcher import Expertise, Nationality, Researcher
 
 __all__ = [
     "Address",
+    "Advisoring",
     "Education",
     "Experience",
     "Expertise",
