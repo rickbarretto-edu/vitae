a2bde05ee9c7bc4733b074d7e32ef3599f788c32
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-07-21 17:13:00 -0300

    Integrate this route with our main module

--- vitae/__main__.py
+++ vitae/__main__.py
@@ -1,8 +1,15 @@
 import cyclopts
+from fastapi import FastAPI
 
 from vitae.features.bootstrap.cli import app as bootstrap_app
 from vitae.features.ingestion.cli import app as ingestion_app
 from vitae.features.ingestion.cli import ingest
+from vitae.features.researchers.routes import router as web_router
+
+
+def web() -> None:
+    app = FastAPI()
+    app.include_router(web_router)
 
 
 def cli() -> None:
