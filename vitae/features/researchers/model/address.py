4427788e291e59625b17b56e06c5c286aa29f2e8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-07-22 20:27:06 -0300

    Create Address model

---
+++ vitae/features/researchers/model/address.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import attrs
+
+if TYPE_CHECKING:
+    from vitae.infra.database import tables as models
+
+
+def force_str(val: str | None) -> str:
+    return val or ""
+
+
+as_title = attrs.converters.pipe(
+    force_str,
+    lambda x: x.title(),
+)
+
+as_upper = attrs.converters.pipe(
+    force_str,
+    lambda x: x.upper(),
+)
+
+
+@attrs.frozen
+class City:
+    name: str = attrs.field(converter=as_title)
+
+
+@attrs.frozen
+class State:
+    abbr: str = attrs.field(converter=as_upper)
+
+
+@attrs.frozen
+class Country:
+    name: str = attrs.field(converter=as_title)
+
+
+@attrs.frozen
+class Address:
+    """Researcher's Address."""
+
+    city: City
+    state: State
+    country: Country
+
+    def __str__(self) -> str:
+        match self:
+            case Address(city, state, country="Brasil"):
+                return f"{city} ({state})"
+            case Address(city, state, country):
+                return f"{city} ({state}), {country}"
+            case _:
+                return ""
+
+    @classmethod
+    def from_database(cls, model: models.Address) -> Address:
+        return cls(
+            city=City(model.city),
+            state=State(model.state),
+            country=Country(model.country),
+        )
