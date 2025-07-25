5a867f125713f3ea091989d3e4dc0ff5a8541df5
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-07-25 17:47:25 -0300

    Add support from database convertion

--- vitae/features/researchers/model/researcher.py
+++ vitae/features/researchers/model/researcher.py
@@ -1,5 +1,10 @@
+from __future__ import annotations
+
 import attrs
 
+from vitae.infra.database import tables
+
+from ._shared import optional
 from .address import Address
 from .cv import Curriculum
 from .external import Lattes, Orcid
@@ -8,10 +13,26 @@ from .external import Lattes, Orcid
 @attrs.frozen
 class ExternalLink:
     lattes: Lattes
-    orcid: Orcid
+    orcid: Orcid | None
+
+    @classmethod
+    def from_table(cls, researcher: tables.Researcher):
+        return cls(
+            lattes=Lattes.from_id(researcher.lattes_id),
+            orcid=optional(researcher.orcid, Orcid.from_url),
+        )
+
 
 @attrs.frozen
 class Researcher:
     links: ExternalLink
     address: Address
     curriculum: Curriculum
+
+    @classmethod
+    def from_table(cls, table: tables.Researcher):
+        return cls(
+            links=ExternalLink.from_table(table),
+            address=Address.from_table(table.address),
+            curriculum=Curriculum.from_table(table),
+        )
