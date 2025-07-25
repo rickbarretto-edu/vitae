d2ea3de22f420c8b04a8a4ff16bc928e102539e9
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-07-25 17:47:49 -0300

    Create basic repository for Researchers

---
+++ vitae/features/researchers/repository/researchers.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Protocol
+
+from sqlmodel import col, select
+
+from vitae.features.researchers.model.researcher import Researcher
+from vitae.infra.database import Database, tables
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+
+class Researchers(Protocol):
+    """Researchers's interface."""
+
+    def by_id(self, lattes_id: str) -> Researcher | None: ...
+    def by_name(self, name: str) -> Iterable[Researcher]: ...
+
+
+class ResearchersInDatabase(Researchers):
+    database: Database
+
+    def by_id(self, lattes_id: str) -> Researcher | None:
+        with self.database.session as session:
+            result = session.exec(
+                select(tables.Researcher).where(
+                    col(tables.Researcher.lattes_id) == lattes_id,
+                ),
+            ).first()
+
+            if result:
+                return Researcher.from_table(result)
+            return None
+
+    def by_name(self, name: str) -> Iterable[Researcher]:
+        return []
