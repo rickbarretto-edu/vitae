64e9e0b15ca0d72a1bb2d9ac2d5a91a9e9a35be6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-07-25 18:14:32 -0300

    Properly integrate Search case with FastAPI

--- vitae/features/researchers/routes.py
+++ vitae/features/researchers/routes.py
@@ -1,6 +1,17 @@
+from __future__ import annotations
+
+from pathlib import Path
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
+
+from vitae.features.researchers.repository.researchers import (
+    ResearchersInDatabase,
+)
+from vitae.features.researchers.usecases.search import SearchResearchers
+from vitae.infra.database import Database
+from vitae.settings.vitae import Vitae
 
 router = APIRouter()
 templates = Jinja2Templates("vitae/features/researchers/templates")
@@ -8,8 +19,25 @@ templates = Jinja2Templates("vitae/features/researchers/templates")
 
 @router.get("/", response_class=HTMLResponse)
 def show_search(request: Request, query: str | None = None):
-    # Usecase go here...
-    return templates.TemplateResponse("search.html", {"request": request})
+    vitae = Vitae.from_toml(Path("vitae.toml"))
+    database = Database(vitae.postgres.engine)
+
+    researchers = ResearchersInDatabase(database)
+    search = SearchResearchers(researchers)
+
+    researchers = (
+        search.by_id(query)
+        if query
+        else []
+    )
+
+    return templates.TemplateResponse(
+        "search.html",
+        {
+            "request": request,
+            "researchers": researchers,
+        },
+    )
 
 
 @router.get("/researcher/{id}", response_class=HTMLResponse)
