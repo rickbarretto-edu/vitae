46c1f9e93957cd78b732a0d12871bf79878057a8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-07-26 18:07:29 -0300

    Document module

--- vitae/features/researchers/model/academic/external.py
+++ vitae/features/researchers/model/academic/external.py
@@ -19,6 +19,11 @@ __all__ = [
 
 @attrs.frozen
 class ExternalLinks:
+    """Researcher's links to external websites.
+
+    Orcid is optional.
+    """
+
     lattes: Lattes
     orcid: Orcid | None
 
@@ -38,8 +43,7 @@ class InvalidURL(ValueError):  # noqa: N818
 class Lattes:
     """Researcher's Lattes.
 
-    Lattes has an ID of the type: 0000000000000000,
-    and also an associated URL.
+    Lattes has an ID of 16-digits and also an associated URL.
     """
 
     id: str = attrs.field(
@@ -56,6 +60,19 @@ class Lattes:
 
     @classmethod
     def from_url(cls, url: str) -> Self:
+        """Lattes given a Lattes' URL.
+
+        A valid URL has "lattes.cnpq.br/{id}"
+
+        Returns
+        -------
+        Self.
+
+        Raises
+        ------
+        InvalidURL
+
+        """
         host = r"lattes\.cnpq\.br"
         match = re.search(rf"{host}/(\d+)", url)
 
@@ -71,7 +88,8 @@ class Lattes:
 class Orcid:
     """Researcher's Orcid.
 
-    Orcid has an ID of the type: 0000-0000-0000-0000,
+    Orcid has an ID of 4 groups of 4-digits separated by hyphen,
+    i.e.: "0000-0000-0000-0000",
     and also an associated URL.
     """
 
@@ -89,6 +107,19 @@ class Orcid:
 
     @classmethod
     def from_url(cls, url: str) -> Self:
+        """Orcid given a Lattes' URL.
+
+        A valid URL has "orcid.org/{id}"
+
+        Returns
+        -------
+        Self
+
+        Raises
+        ------
+        InvalidURL
+
+        """
         host = r"orcid\.org"
         match = re.search(rf"{host}/([\d\-]+)", url)
 
