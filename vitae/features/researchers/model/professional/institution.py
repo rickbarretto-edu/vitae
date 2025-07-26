bf2c36d6a9cfdd2f3f54d5a75644d5cc86f59be1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-07-26 17:43:22 -0300

    Document and Enhance Code

--- vitae/features/researchers/model/professional/institution.py
+++ vitae/features/researchers/model/professional/institution.py
@@ -1,8 +1,12 @@
+"""Institution related domain objects."""
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Self
 
 import attrs
+
+from vitae.features.researchers.lib import optional
 
 if TYPE_CHECKING:
     from vitae.infra.database import tables
@@ -10,15 +14,32 @@ if TYPE_CHECKING:
 
 @attrs.frozen
 class LinkedInstitution:
+    """Researcher's Linked Institution.
+
+    A Researcher may be associated with certain Institution.
+    """
+
     _name: str
 
     @property
     def name(self) -> str:
+        """Formatted Institution's name."""
         return self._name.title()
-    
+
     def __str__(self) -> str:
         return self.name
 
     @classmethod
-    def from_table(cls, address: tables.Address) -> Self:
-        return cls(address.institution.name)
+    def from_table(cls, address: tables.Address) -> Self | None:
+        """Linked Institution from given Address.
+
+        Returns
+        -------
+        Itself if Address has an institution.
+
+        """
+        institution_name: str | None = optional(
+            address.institution,
+            lambda x: x.name,
+        )
+        return optional(institution_name, cls)
