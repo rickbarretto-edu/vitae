ef4781d82415d540945a929358ffed47b949d0c6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-07-26 16:56:14 -0300

    Link Address to Institution

--- vitae/infra/database/tables/institution.py
+++ vitae/infra/database/tables/institution.py
@@ -2,7 +2,12 @@
 
 # ruff: noqa: FA102, D101
 
-from .orm import Orm, required_key
+from typing import TYPE_CHECKING
+
+from .orm import Orm, link, required_key
+
+if TYPE_CHECKING:
+    from .professional import Address
 
 __all__ = ["Institution"]
 
@@ -13,3 +18,6 @@ class Institution(Orm, table=True):
     country: str | None
     state: str | None
     abbr: str | None
+
+    addresses: list["Address"] = link("institution")
+
