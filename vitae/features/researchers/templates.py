e135052261778c5a8ca31a532380e50fdc4e5df5
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-01 15:23:53 -0300

    Use custom Environment
    This also makes sure this is correctly using UTF-8

--- vitae/features/researchers/templates.py
+++ vitae/features/researchers/templates.py
@@ -1,4 +1,5 @@
 from fastapi.templating import Jinja2Templates
+from jinja2 import Environment, FileSystemLoader
 import jinjax
 
 __all__ = ["templates"]
@@ -6,15 +7,24 @@ __all__ = ["templates"]
 
 # =~=~=~=~=~=~= Filters =~=~=~=~=~=~=
 
+
 def words(text: str) -> list[str]:
     return text.split(" ")
 
 
 # =~=~=~=~=~=~= Jinja Setup =~=~=~=~=~=~=
 
-templates = Jinja2Templates("vitae/features/researchers/templates")
-templates.env.filters["words"] = words
+env = Environment(
+    loader=FileSystemLoader(
+        "vitae/features/researchers/templates",
+        encoding="utf-8",
+    ),
+)
+env.filters["words"] = words
+env.add_extension(jinjax.JinjaX)
 
-templates.env.add_extension(jinjax.JinjaX)
-catalog = jinjax.Catalog(jinja_env=templates.env)
+templates = Jinja2Templates(directory="vitae/features/researchers/templates")
+templates.env = env
+
+catalog = jinjax.Catalog(jinja_env=env)
 catalog.add_folder("vitae/features/researchers/templates")
