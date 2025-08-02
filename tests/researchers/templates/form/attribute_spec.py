f11b5728c0f63ed32964e71edd30c40b56218b4c
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-02 15:45:43 -0300

    Use bs4

--- tests/researchers/templates/form/attribute_spec.py
+++ tests/researchers/templates/form/attribute_spec.py
@@ -1,23 +1,29 @@
+from bs4 import BeautifulSoup, Tag
 import pytest
 from jinjax import HTMLAttrs
 
+
 @pytest.fixture(scope="module")
 def attribute(catalog):
-    return catalog.render(
+    component = catalog.render(
         "form.Attribute",
         _attrs=HTMLAttrs({"class": "flex flex-col gap-2"}),
         _content="<label>Label</label><input placeholder='Search'/>",
     )
+    return BeautifulSoup(component, "html.parser")
 
 class DescribeFormAttribute:
 
     def has_content(self, attribute):
-        assert "<label>Label</label>" in attribute
-        assert "<input placeholder='Search'" in attribute
+        assert attribute.find("label").text == "Label" # type: ignore
+        assert attribute.find("input")["placeholder"] == "Search" # type: ignore
 
     def has_custom_class(self, attribute):
-        assert 'class="flex flex-col gap-2"' in attribute
+        classes = attribute.find("div")["class"] # type: ignore
+        
+        assert "flex" in classes
+        assert "flex-col" in classes
+        assert "gap-2" in classes
 
     def is_a_div(self, attribute):
-        assert attribute.strip().startswith("<div")
-        assert attribute.strip().endswith("</div>")
+        assert attribute.find("div").name == "div" # type: ignore
