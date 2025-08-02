1cc936120095cc5c8033aaf20491a8de35328d7e
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-02 12:35:55 -0300

    Fix test

--- vitae/features/researchers/model/academic/_expertises_spec.py
+++ vitae/features/researchers/model/academic/_expertises_spec.py
@@ -20,9 +20,17 @@ class DescribeStudyField:
 class DescribeExpertises:
     def can_be_created_from_tables(self):
 
+        def new_expertise(major, area, sub):
+            return tables.Expertise(
+                id=1, researcher_id="0",
+                major=major, 
+                area=area, 
+                sub=sub,
+            )
+
         expertises = [
-            tables.Expertise("major1", "area1", "sub1"),
-            tables.Expertise("major2", "area2", None),
+            new_expertise("major1", "area2", "sub1"),
+            new_expertise("major2", "area2", None),
         ]
 
         expertises_obj = Expertises.from_tables(expertises)
