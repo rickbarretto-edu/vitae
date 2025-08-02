c5c0976a610a9f72ec49228f6fad65b6cb8f54d1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-02 12:31:04 -0300

    Test ExternalLinks, Lattes and Orcid

---
+++ vitae/features/researchers/model/academic/_external_spec.py
@@ -0,0 +1,66 @@
+
+from vitae.infra.database import tables
+
+from .external import ExternalLinks, Lattes, Orcid, InvalidURL
+
+
+class DescribeLattes:
+    def is_created_from_valid_id(self):
+        lattes = Lattes.from_id("1234567890123456")
+        assert lattes.id == "1234567890123456"
+        assert lattes.url == "http://lattes.cnpq.br/1234567890123456"
+
+    def raises_invalid_url_for_wrong_url(self):
+        import pytest
+        with pytest.raises(InvalidURL):
+            Lattes.from_url("http://invalid.url/1234567890123456")
+
+    def is_created_from_valid_url(self):
+        url = "http://lattes.cnpq.br/1234567890123456"
+        lattes = Lattes.from_url(url)
+        assert lattes.id == "1234567890123456"
+        assert lattes.url == url
+
+
+class DescribeOrcid:
+    def is_created_from_valid_id(self):
+        orcid = Orcid.from_id("0000-0000-0000-0000")
+        assert orcid.id == "0000-0000-0000-0000"
+        assert orcid.url == "http://orcid.org/0000-0000-0000-0000"
+
+    def raises_invalid_url_for_wrong_url(self):
+        import pytest
+        with pytest.raises(InvalidURL):
+            Orcid.from_url("http://invalid.org/0000-0000-0000-0000")
+
+    def is_created_from_valid_url(self):
+        url = "http://orcid.org/0000-0000-0000-0000"
+        orcid = Orcid.from_url(url)
+        assert orcid.id == "0000-0000-0000-0000"
+        assert orcid.url == url
+
+
+class DescribeExternalLinks:
+    def can_be_created_from_table(self):
+        researcher = tables.Researcher(
+            lattes_id="1234567890123456",
+            full_name="John Doe",
+            orcid="http://orcid.org/0000-0002-1825-0097"
+        )
+        links = ExternalLinks.from_table(researcher)
+
+        assert isinstance(links.lattes, Lattes)
+        assert links.lattes.id == "1234567890123456"
+        assert isinstance(links.orcid, Orcid)
+        assert links.orcid.id == "0000-0002-1825-0097"
+
+    def can_be_created_from_table_with_no_orcid(self):
+        researcher = tables.Researcher(
+            lattes_id="1234567890123456",
+            full_name="John Doe",
+        )
+        links = ExternalLinks.from_table(researcher)
+
+        assert isinstance(links.lattes, Lattes)
+        assert links.lattes.id == "1234567890123456"
+        assert links.orcid is None
