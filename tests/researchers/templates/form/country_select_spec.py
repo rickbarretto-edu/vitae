d79a34227a94e31434851a65317466abc7d43dc3
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-02 18:34:15 -0300

    Test CountrySelect

---
+++ tests/researchers/templates/form/country_select_spec.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup, Tag
+import pytest
+from jinjax import HTMLAttrs
+
+
+@pytest.fixture(scope="module")
+def select(catalog):
+    component = catalog.render(
+        "form.CountrySelect",
+        on_url="country",
+        available=["brazil", "canada", "costa rica"],
+        _content="Countries",
+    )
+    return BeautifulSoup(component, "html.parser")
+
+class DescribeButton:
+
+    def has_4_options(self, select):
+        options = select.find_all("option")
+        assert 4 == len(options)
+    
+    def its_options_has_given_values(self, select):
+        options = select.find_all("option")
+
+        expected = ["", "brazil", "canada", "costa rica"]
+        actual = [option["value"].strip() for option in options]
+
+        assert expected == actual
+
+    def its_options_display_is_titled(self, select):
+        options = select.find_all("option")
+
+        expected = ["Countries", "Brazil", "Canada", "Costa Rica"]
+        actual = [option.text.strip() for option in options]
+
+        assert expected == actual
+
+    def is_select(self, select):
+        assert "select" == select.find("select").name
+
+    def its_bind_to_url(self, select):
+        assert "country" == select.find("select")["name"]
