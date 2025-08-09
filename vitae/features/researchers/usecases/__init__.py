cbe13b99226586d8d96c549651d8dc690ef238eb
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-09 00:20:46 -0300

    Export feature

--- vitae/features/researchers/usecases/__init__.py
+++ vitae/features/researchers/usecases/__init__.py
@@ -1,4 +1,5 @@
+from .export import ExportToLucy
 from .filters import LoadFilters
 from .search import SearchResearchers, SortingOrder
 
-__all__ = ["LoadFilters", "SearchResearchers", "SortingOrder"]
+__all__ = ["ExportToLucy", "LoadFilters", "SearchResearchers", "SortingOrder"]
