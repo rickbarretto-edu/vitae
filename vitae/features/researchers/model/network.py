8f85ccb0c99a5466c168b41650350de41cdaf84f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-08 22:09:05 -0300

    Add internal helper classes

--- vitae/features/researchers/model/network.py
+++ vitae/features/researchers/model/network.py
@@ -9,11 +9,38 @@ if TYPE_CHECKING:
 
 
 @attrs.frozen
+class ResearcherAsNode:
+    _itself: Researcher
+
+    @property
+    def name(self) -> str:
+        full_name = self._itself.this.name.each
+        return "".join(
+            [
+                full_name[-1],
+                *[name[0] for name in full_name[:-1]],
+            ],
+        )
+
+    @property
+    def lattes_id(self) -> str:
+        return self._itself.links.lattes.id
+
+
+@attrs.frozen
+class AdvisingLink:
+    """Linking between two researchers."""
+
+    student: ResearcherAsNode
+    advisor: ResearcherAsNode
+
+
+@attrs.frozen
 class Network:
     """Network of Researchers with a central Researcher.
 
     On DSA view, this is a graph, where `_of` is the root.
     """
 
-    of: Researcher
+    of: ResearcherAsNode
     links_to: list[Network]
