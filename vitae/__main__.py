cf9a34d585fa161db2b5b537b417a674943f134b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-09 19:19:27 -0300

    Integrate Researchers with main CLI

--- vitae/__main__.py
+++ vitae/__main__.py
@@ -4,11 +4,13 @@ import uvicorn
 
 from vitae.features.bootstrap.cli import app as bootstrap_app
 from vitae.features.ingestion.cli import app as ingestion_app
+from vitae.features.researchers.cli import app as researchers_app
 
 def cli() -> None:
     app = cyclopts.App(name="vitae")
     app.command(ingestion_app)
     app.command(bootstrap_app)
+    app.command(researchers_app)
     app()
 
 
