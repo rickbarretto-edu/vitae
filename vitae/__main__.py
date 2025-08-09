bfd6f2a6c128cd2b4a8ec19868aecbc733899287
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-09 19:20:29 -0300

    Document function

--- vitae/__main__.py
+++ vitae/__main__.py
@@ -7,6 +7,7 @@ from vitae.features.ingestion.cli import app as ingestion_app
 from vitae.features.researchers.cli import app as researchers_app
 
 def cli() -> None:
+    """Integrates all features into one CLI."""
     app = cyclopts.App(name="vitae")
     app.command(ingestion_app)
     app.command(bootstrap_app)
