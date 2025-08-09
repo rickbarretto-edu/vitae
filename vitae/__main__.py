ed5dae8b61e0937e51ff49b61adb1a054a997c7a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-09 19:14:06 -0300

    Remove debugging code

--- vitae/__main__.py
+++ vitae/__main__.py
@@ -4,19 +4,6 @@ import uvicorn
 
 from vitae.features.bootstrap.cli import app as bootstrap_app
 from vitae.features.ingestion.cli import app as ingestion_app
-from vitae.features.ingestion.cli import ingest
-from vitae.features.researchers.routes import router as web_router
-
-
-def web_new() -> FastAPI:
-    app = FastAPI()
-    app.include_router(web_router)
-    return app
-
-
-def web() -> None:
-    uvicorn.run("vitae.__main__:web_new", factory=True, reload=True)
-
 
 def cli() -> None:
     app = cyclopts.App(name="vitae")
@@ -25,9 +12,5 @@ def cli() -> None:
     app()
 
 
-def debug_ingest() -> None:
-    ingest(buffer=5)
-
-
 if __name__ == "__main__":
-    web()
+    cli()
