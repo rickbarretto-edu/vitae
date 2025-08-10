337ae146613423bc3e94d3b21777adf13c869bec
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-10 15:35:09 -0300

    Remove create TOML file feature
    This should not be created from here, but bundled by the build script.

--- vitae/app.py
+++ vitae/app.py
@@ -18,29 +18,6 @@ def close_splash(delay=5):
 
     threading.Thread(target=delayed_close, daemon=True).start()
 
-def create_config_file():
-    example = """
-[project]
-in_production = false
-
-[postgres.user]
-name = "postgres_user"
-password = "your_secure_password"
-
-[postgres.database]
-host = "localhost"
-port = 5432
-name = "your_database_name"
-flush_every = 100
-
-[paths]
-curricula = "path/to/your/curricula/files"
-    """
-    config_file = Path("vitae.toml")
-    if not config_file.exists():
-        with config_file.open("+w", encoding="utf-8") as file:
-            file.write(example)
-
 
 def redirect_console_to_logfile(logfile: Path):
     log_file = logfile.open("+a", encoding="utf-8")
@@ -53,6 +30,5 @@ def start_app():
 
 if __name__ == "__main__":
     redirect_console_to_logfile(Path("vitae.log"))
-    create_config_file()
     close_splash()
     start_app()
