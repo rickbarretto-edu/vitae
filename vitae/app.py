c1bf4f4915ef0761447f0cbc223fc8bb80468f4b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-10 12:58:06 -0300

    Create eecutable entry

--- vitae/app.py
+++ vitae/app.py
@@ -1,7 +1,19 @@
+import io
+import sys
 
 from flaskwebgui import FlaskUI
 
 from vitae.features.researchers.app import main
 
-if __name__ == "__main__":
+
+def suppress_console():
+    sys.stdout = io.StringIO()
+    sys.stderr = io.StringIO()
+
+def start_app():
     FlaskUI(app=main(), server="fastapi").run()
+
+
+if __name__ == "__main__":
+    # suppress_console()
+    start_app()
--- vitae/features/researchers/templates/__init__.py
+++ vitae/features/researchers/templates/__init__.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from fastapi.templating import Jinja2Templates
 from jinja2 import Environment, FileSystemLoader
 import jinjax
@@ -14,19 +17,26 @@ def words(text: str) -> list[str]:
 
 # =~=~=~=~=~=~= Jinja Setup =~=~=~=~=~=~=
 
+# Changes the base-Path when this is compiled by PyIntaller
+if getattr(sys, "frozen", False):
+    base_path = getattr(sys, "_MEIPASS", ".")
+else:
+    base_path = os.path.abspath(".")
+
+
+templates_directory = os.path.join(base_path, "vitae/features/researchers/templates")
+
+
 env = Environment(
-    loader=FileSystemLoader(
-        "vitae/features/researchers/templates",
-        encoding="utf-8",
-    ),
+    loader=FileSystemLoader(templates_directory, encoding="utf-8"),
     autoescape=True,
     auto_reload=True,
 )
 env.filters["words"] = words
 env.add_extension(jinjax.JinjaX)
 
-templates = Jinja2Templates(directory="vitae/features/researchers/templates")
+templates = Jinja2Templates(directory=templates_directory)
 templates.env = env
 
 catalog = jinjax.Catalog(jinja_env=env)
-catalog.add_folder("vitae/features/researchers/templates")
+catalog.add_folder(templates_directory)
