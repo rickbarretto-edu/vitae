7ebc7a47fcbc2434850443ef079ad3da11e00fe2
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-27 23:22:34 -0300

    The query must be unique

--- vitae/features/researchers/repository/researchers.py
+++ vitae/features/researchers/repository/researchers.py
@@ -59,6 +59,7 @@ def using_filter(
             .where(col(tables.Nationality.born_country) == filters["country"]
                 if haskey(filters, "country") else True
             )
+            .unique()
     )
 
 # fmt: on
