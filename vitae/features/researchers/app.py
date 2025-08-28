f0a5eada6742ca706439311654634fd5ee33e3ef
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-08-27 21:11:17 -0300

    Use print instead of loguru

--- vitae/features/researchers/app.py
+++ vitae/features/researchers/app.py
@@ -13,15 +13,14 @@ def main():
     async def log_request_runtime(request: Request, call_next):
         start_time = time.perf_counter()
         response = await call_next(request)
-        process_time = (time.perf_counter() - start_time) * 1000  # ms
+        process_time_in_ms = (time.perf_counter() - start_time) * 1000
         
-        logger.info(
-            "{method} {path} - {status_code} - {time:.2f} ms",
+        print("{method} {path} | Done in: {time:.2f} ms".format(
             method=request.method,
             path=request.url.path,
             status_code=response.status_code,
-            time=process_time,
-        )
+            time=process_time_in_ms,
+        ))
         return response
 
 
